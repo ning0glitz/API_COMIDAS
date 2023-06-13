06b25bae0f4757d7b4aea462c8a7fd13b182459e
ning0glitz <75601700@certus.edu.pe>
2023-06-13 18:57:41 -0500

    API_REST_COMIDAS

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:api_2_flutter/views/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food recipe',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        primaryColor: Colors.white,
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.white),
+        ),
+      ),
+      home: HomePage(),
+    );
+  }
+}
